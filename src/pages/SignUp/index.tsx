cd41c790520fa25f274f4c7ba6d2bfcde58d66e7
Jonas Castro <jonascastro128@gmail.com>
2020-07-09 16:37:39 -0300

    create error tooltips

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -23,7 +23,7 @@ const SignUp: React.FC = () => {
         email: Yup.string()
           .required('E-mail obrigatório')
           .email('Digite um e-mail válido'),
-        password: Yup.string().min(6, 'no mínimo 6 digitos'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
       });
 
       await schema.validate(data, {
